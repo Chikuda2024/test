09bc962a1d7424b31e21761e55628f09c29522b5
chikuda <taka2024.1@gmail.com>
2024-11-19 17:21:16 +0900

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 竹田 信夫 on 2024/11/19.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
